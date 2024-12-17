f8d4e8d0b5d145f082375e61e848313a98c10417
honam <75104291+honambsn@users.noreply.github.com>
2024-12-17 23:01:18 +0700

    update: User Bootstrap Template Integration

--- Online Food/Online Food/User/User.Master.cs
+++ Online Food/Online Food/User/User.Master.cs
@@ -9,9 +9,30 @@ namespace Online_Food.User
 {
 	public partial class User : System.Web.UI.MasterPage
 	{
-		protected void Page_Load(object sender, EventArgs e)
+		protected void Page_Init(object sender, EventArgs e)
 		{
 
+		}
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+			{
+				form1.Attributes.Add("class", "sub_page");
+			}
+			else
+			{
+				form1.Attributes.Remove("class");
+
+				//load control
+				Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl1.ascx");
+
+
+				//add the control to the panel
+				pnlSliderUC.Controls.Add(sliderUserControl);
+			}
+
+			
 		}
 	}
 }
