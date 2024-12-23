06aacff226902f550371d33077dd0fb19fe4e35c
honam <75104291+honambsn@users.noreply.github.com>
2024-12-23 22:28:57 +0700

    update: create category in admin module

---
+++ Online Food/Online Food/Admin/Category.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Online_Food.Admin
+{
+	public partial class Category : System.Web.UI.Page
+	{
+		SqlConnection cn;
+		SqlCommand cmd;
+		SqlDataReader dr;
+		DataTable dt;
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnAddOrUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
