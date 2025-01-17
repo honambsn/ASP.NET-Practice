09099292839a4246ac94385fc61600b9b130973a
honam <75104291+honambsn@users.noreply.github.com>
2025-01-17 23:02:42 +0700

    update: user registration crud completed

--- Online Food/Online Food/User/Registration.aspx.cs
+++ Online Food/Online Food/User/Registration.aspx.cs
@@ -18,7 +18,22 @@ namespace Online_Food.User
 		DataTable dt;
 		protected void Page_Load(object sender, EventArgs e)
 		{
-
+			if (!IsPostBack)
+			{
+				if (Request.QueryString["id"] != null)// && Session["UserID"] != null)
+				{
+					getUserDetails();
+				}
+				else if (Session["UserID"] != null)
+				{
+					Response.Redirect("Default.aspx");
+				}
+				else
+				{
+					lblHeaderMsg.Text = "<h2>Register</h2>";
+					lblAlreadyUser.Text = "Already a user? <a href='Login.aspx'>Login</a>";
+				}
+			}
 		}
 
         protected void btnRegister_Click(object sender, EventArgs e)
@@ -30,7 +45,7 @@ namespace Online_Food.User
 			cmd = new SqlCommand("User_Crud", con);
 			cmd.Parameters.AddWithValue("@Action", userID == 0 ? "INSERT" : "UPDATE");
 			cmd.Parameters.AddWithValue("@UserID", userID);
-			cmd.Parameters.AddWithValue("@Name", txtUsername.Text.Trim());
+			cmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
 			cmd.Parameters.AddWithValue("@Username", txtUsername.Text.Trim());
 			cmd.Parameters.AddWithValue("@Mobile", txtMobile.Text.Trim());
 			cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
@@ -75,8 +90,8 @@ namespace Online_Food.User
 
 					cmd.ExecuteNonQuery();
 					actionName = userID == 0 ?
-						"Registered! <b><a> href='Login.aspx'>Click here</a></b> to do Login":
-						"Profile Updated! <b><a> href='Profile.aspx'>Can check here</a></b>";
+						" Registered! <b><a href='Login.aspx'>Click here</a></b> to do Login":
+						" Profile Updated! <b><a href='Profile.aspx'>Can check here</a></b>";
 
 					lblMsg.Visible = true;
 					lblMsg.Text = "<b>" + txtUsername.Text.Trim() + "</b>" + actionName;
@@ -108,6 +123,43 @@ namespace Online_Food.User
 					con.Close();
 				}
 			}
+		}
+
+		void getUserDetails()
+		{
+			con = new SqlConnection(Connection.GetConnectionString());
+			cmd = new SqlCommand("User_Crud", con);
+			cmd.Parameters.AddWithValue("@Action", "SELECT4PROFILE");
+			cmd.Parameters.AddWithValue("@UserID", Request.QueryString["id"]);
+			cmd.CommandType = CommandType.StoredProcedure;
+			sda = new SqlDataAdapter(cmd);
+			dt = new DataTable();
+			sda.Fill(dt);
+			if (dt.Rows.Count == 1)
+			{
+				txtName.Text = dt.Rows[0]["Name"].ToString();
+				txtUsername.Text = dt.Rows[0]["Username"].ToString();
+				txtMobile.Text = dt.Rows[0]["Mobile"].ToString();
+				txtEmail.Text = dt.Rows[0]["Email"].ToString();
+				txtAddress.Text = dt.Rows[0]["Address"].ToString();
+				txtPostCode.Text = dt.Rows[0]["PostCode"].ToString();
+				imgUser.ImageUrl = string.IsNullOrEmpty(dt.Rows[0]["ImageUrl"].ToString())
+					? "../Images/No_image.png" : "../" + dt.Rows[0]["ImageUrl"].ToString();
+				imgUser.Height = 200;
+				imgUser.Width = 200;
+				txtPassword.TextMode = TextBoxMode.SingleLine;
+				txtPassword.ReadOnly = true;
+				txtPassword.Text = dt.Rows[0]["Password"].ToString();
+			}
+			else
+			{
+				lblHeaderMsg.Text = "<h2>Invalid User!</h2>";
+				lblAlreadyUser.Text = "User not found!";
+			}
+
+			lblHeaderMsg.Text = "<h2>Update Profile</h2>";
+			btnRegister.Text = "Update";
+			lblAlreadyUser.Text = "";
 		}
 
 		private void clear()
