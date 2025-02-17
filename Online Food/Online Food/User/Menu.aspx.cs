ef002b8405f1afd33f35fdfac4cce21f732ec8f7
honam <75104291+honambsn@users.noreply.github.com>
2025-02-17 22:16:50 +0700

    update: create menu category, product

--- Online Food/Online Food/User/Menu.aspx.cs
+++ Online Food/Online Food/User/Menu.aspx.cs
@@ -1,17 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Online_Food.Admin;
 
 namespace Online_Food.User
 {
 	public partial class Menu : System.Web.UI.Page
 	{
+		SqlConnection con;
+		SqlCommand cmd;
+		SqlDataAdapter sda;
+		DataTable dt;
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			if (!IsPostBack)
+			{
+				getCategories();
+				getProducts();
+			}
+		}
+
+		private void getCategories()
+		{
+			using (SqlConnection cm = new SqlConnection(Connection.GetConnectionString()))
+			{
+				if (cm.State == ConnectionState.Closed)
+				{
+					cm.Open();
+				}
+
+				using (SqlCommand cmd = new SqlCommand("Category_Crud", cm))
+				{
+					cmd.Parameters.AddWithValue("@Action", "ACTIVECAT");
+					cmd.CommandType = CommandType.StoredProcedure;
+					using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+					{
+						DataTable dt = new DataTable();
+						sda.Fill(dt);
+
+						// Bind the result to the rptCategory control
+						rCategory.DataSource = dt;
+						rCategory.DataBind();
+					}
+				}
+			}
 
 		}
+
+
+		private void getProducts()
+		{
+			using (SqlConnection cm = new SqlConnection(Connection.GetConnectionString()))
+			{
+				if (cm.State == ConnectionState.Closed)
+				{
+					cm.Open();
+				}
+
+				using (SqlCommand cmd = new SqlCommand("Product_Crud", cm))
+				{
+					cmd.Parameters.AddWithValue("@Action", "ACTIVEPROD");
+					cmd.CommandType = CommandType.StoredProcedure;
+					using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+					{
+						DataTable dt = new DataTable();
+						sda.Fill(dt);
+
+						// Bind the result to the rProduct control
+						rProducts.DataSource = dt;
+						rProducts.DataBind();
+					}
+				}
+			}
+
+		}
+
+		public string LowerCase(object obj)
+		{
+			return obj.ToString().ToLower();
+		}
+
 	}
 }
