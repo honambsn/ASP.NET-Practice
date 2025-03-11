4e7ca9bae8f4496d45604c83b595434e7e4dc076
honam <75104291+honambsn@users.noreply.github.com>
2025-03-11 22:16:53 +0700

    update: rewrite orderpayment

--- Online Food/Online Food/User/Payment.aspx.cs
+++ Online Food/Online Food/User/Payment.aspx.cs
@@ -85,79 +85,164 @@ namespace Online_Food.User
 				new DataColumn("OrderDate", typeof(DateTime)),
 			});
 
-			//con = new SqlConnection(Connection.GetConnectionString());
-			//if (con.State == ConnectionState.Closed)
-			//{
-			//	con.Open();
-			//}
+			con = new SqlConnection(Connection.GetConnectionString());
+			con.Open();
+			transaction = con.BeginTransaction();
 
-			using (SqlConnection cm = new SqlConnection(Connection.GetConnectionString()))
+			con = new SqlConnection(Connection.GetConnectionString());
+			cmd = new SqlCommand("Save_Payment", con, transaction);
+			cmd.CommandType = CommandType.StoredProcedure;
+			cmd.Parameters.AddWithValue("@Name", name);
+			cmd.Parameters.AddWithValue("@CardNo", cardNo);
+			cmd.Parameters.AddWithValue("@ExpDate", expDate);
+			cmd.Parameters.AddWithValue("@Cvv", cvv);
+			cmd.Parameters.AddWithValue("@Address", address);
+			cmd.Parameters.AddWithValue("@PaymentMode", paymentMode);
+			cmd.Parameters.Add("@InsertedID", SqlDbType.Int);
+			cmd.Parameters["@InsertedID"].Direction = ParameterDirection.Output;
+			try
 			{
-				if (cm.State == ConnectionState.Closed)
+				cmd.ExecuteNonQuery();
+				paymentID = Convert.ToInt32(cmd.Parameters["@InsertedID"].Value);
+
+				cmd = new SqlCommand("Cart_Crud", con);
+				cmd.Parameters.AddWithValue("@Action", "SELECT");
+				cmd.Parameters.AddWithValue("UserID", Session["UserID"]);
+				cmd.CommandType = CommandType.StoredProcedure;
+				dr = cmd.ExecuteReader();
+				
+				while (dr.Read())
 				{
-					cm.Open();
+					productID = Convert.ToInt32(dr["ProductID"]);
+					quantity = Convert.ToInt32(dr["Quantity"]);
+
+					// update product quantity
+
+					// delete cart item
 				}
-
-				using (SqlCommand cmd = new SqlCommand("Save_Payment", cm))
+			}
+			catch (Exception ex)
+			{
+				try
 				{
-					cmd.Parameters.AddWithValue("@Name", name);
-					cmd.Parameters.AddWithValue("@CardNo", cardNo);
-					cmd.Parameters.AddWithValue("@ExpDate", expDate);
-					cmd.Parameters.AddWithValue("@Cvv", cvv);
-					cmd.Parameters.AddWithValue("@Address", address);
-					cmd.Parameters.AddWithValue("@PaymentMode", paymentMode);
-					cmd.Parameters.Add("@InsertedID", SqlDbType.Int);
-					cmd.Parameters["@InsertedID"].Direction = ParameterDirection.Output;
-
-					cmd.CommandType = CommandType.StoredProcedure;
-
-					try
-					{
-						cmd.ExecuteNonQuery();
-						paymentID = Convert.ToInt32(cmd.Parameters["@InsertedID"].Value);
-
-						using (SqlConnection cm1 = new SqlConnection(Connection.GetConnectionString()))
-						{
-							if (cm1.State == ConnectionState.Closed)
-							{
-								cm1.Open();
-							}
-
-							using (SqlCommand cmd1 = new SqlCommand("Cart_Crud", cm1))
-							{
-								cmd1.Parameters.AddWithValue("@Action", "SELECT");
-								cmd1.Parameters.AddWithValue("UserID", Session["UserID"]);
-								cmd1.CommandType = CommandType.StoredProcedure;
-
-								dr = cmd1.ExecuteReader();
-								while (dr.Read())
-								{
-									productID = Convert.ToInt32(dr["ProductID"]);
-									//productID = (int)dr["ProductID"];
-									quantity = Convert.ToInt32(dr["Quantity"]);
-
-
-									dt.Rows.Add(dr["OrderNo"], productID, quantity, Session["UserID"], 1, DateTime.Now);
-								}
-							}
-						}
-					}
-					catch (Exception ex)
-					{
-						throw ex;
-					}
-					finally
-					{
-						if (con.State == ConnectionState.Open)
-						{
-							con.Close();
-						}
-					}
-
-
+					transaction.Rollback();
+				}
+				catch (Exception ex2)
+				{
+					Response.Write("<script>alert('"+ex2.Message+"');</script>");
+				}
+			}
+			finally
+			{
+				if (con.State == ConnectionState.Open)
+				{
+					con.Close();
 				}
 			}
 		}
+
+
+	//	void OrderPayment(string name, string cardNo, string expDate, string cvv, string address, string paymentMode)
+	//	{
+	//		int paymentID;
+	//		int productID;
+	//		int quantity;
+	//		DataTable dt = new DataTable();
+
+	//		// Cấu hình các cột của DataTable
+	//		dt.Columns.AddRange(new DataColumn[] {
+	//	new DataColumn("OrderNo", typeof(string)),
+	//	new DataColumn("ProductID", typeof(int)),
+	//	new DataColumn("Quantity", typeof(int)),
+	//	new DataColumn("UserID", typeof(int)),
+	//	new DataColumn("Status", typeof(int)),
+	//	new DataColumn("OrderDate", typeof(DateTime)),
+	//});
+
+	//		try
+	//		{
+	//			// Sử dụng using để tự động quản lý kết nối và giao dịch
+	//			using (con = new SqlConnection(Connection.GetConnectionString()))
+	//			{
+	//				con.Open();
+	//				using (transaction = con.BeginTransaction())
+	//				{
+	//					// Lưu thông tin thanh toán vào cơ sở dữ liệu
+	//					using (cmd = new SqlCommand("Save_Payment", con, transaction))
+	//					{
+	//						cmd.CommandType = CommandType.StoredProcedure;
+	//						cmd.Parameters.AddWithValue("@Name", name);
+	//						cmd.Parameters.AddWithValue("@CardNo", cardNo);
+	//						cmd.Parameters.AddWithValue("@ExpDate", expDate);
+	//						cmd.Parameters.AddWithValue("@Cvv", cvv);
+	//						cmd.Parameters.AddWithValue("@Address", address);
+	//						cmd.Parameters.AddWithValue("@PaymentMode", paymentMode);
+	//						cmd.Parameters.Add("@InsertedID", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+	//						// Thực thi câu lệnh Stored Procedure
+	//						cmd.ExecuteNonQuery();
+	//						paymentID = Convert.ToInt32(cmd.Parameters["@InsertedID"].Value);
+	//					}
+
+	//					// Lấy thông tin giỏ hàng và xử lý chúng
+	//					using (cmd = new SqlCommand("Cart_Crud", con, transaction))
+	//					{
+	//						cmd.Parameters.AddWithValue("@Action", "SELECT");
+	//						cmd.Parameters.AddWithValue("UserID", Session["UserID"]);
+	//						cmd.CommandType = CommandType.StoredProcedure;
+
+	//						using (dr = cmd.ExecuteReader())
+	//						{
+	//							while (dr.Read())
+	//							{
+	//								productID = Convert.ToInt32(dr["ProductID"]);
+	//								quantity = Convert.ToInt32(dr["Quantity"]);
+
+	//								// Cập nhật số lượng sản phẩm
+	//								using (cmd = new SqlCommand("Update_Product_Quantity", con, transaction))
+	//								{
+	//									cmd.Parameters.AddWithValue("@ProductID", productID);
+	//									cmd.Parameters.AddWithValue("@Quantity", quantity);
+	//									cmd.CommandType = CommandType.StoredProcedure;
+	//									cmd.ExecuteNonQuery();
+	//								}
+
+	//								// Xóa sản phẩm khỏi giỏ hàng
+	//								using (cmd = new SqlCommand("Delete_Cart_Item", con, transaction))
+	//								{
+	//									cmd.Parameters.AddWithValue("@ProductID", productID);
+	//									cmd.Parameters.AddWithValue("@UserID", Session["UserID"]);
+	//									cmd.CommandType = CommandType.StoredProcedure;
+	//									cmd.ExecuteNonQuery();
+	//								}
+	//							}
+	//						}
+	//					}
+
+	//					// Commit giao dịch nếu mọi thao tác thành công
+	//					transaction.Commit();
+	//				}
+	//			}
+	//		}
+	//		catch (Exception ex)
+	//		{
+	//			// Xử lý lỗi và rollback giao dịch nếu có lỗi
+	//			try
+	//			{
+	//				// Nếu có lỗi, rollback giao dịch
+	//				transaction?.Rollback();
+	//			}
+	//			catch (Exception ex2)
+	//			{
+	//				// Hiển thị lỗi khi rollback thất bại
+	//				Response.Write("<script>alert('Lỗi khi rollback: " + ex2.Message + "');</script>");
+	//			}
+
+	//			// Hiển thị lỗi chính
+	//			Response.Write("<script>alert('Lỗi: " + ex.Message + "');</script>");
+	//		}
+	//	}
+
 
 	}
 }
