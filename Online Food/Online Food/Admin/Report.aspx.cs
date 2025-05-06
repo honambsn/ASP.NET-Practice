a92f9146542be268456a907ed795f37ec4421386
honam <75104291+honambsn@users.noreply.github.com>
2025-05-06 23:01:24 +0700

    update: store procedure

--- Online Food/Online Food/Admin/Report.aspx.cs
+++ Online Food/Online Food/Admin/Report.aspx.cs
@@ -28,17 +28,50 @@ namespace Online_Food.Admin
 			}
 		}
 
-		private void getContacts()
+		private void getReportData(DateTime fromDate, DateTime toDate)
 		{
-			con =  new SqlConnection(Connection.GetConnectionString());
+			double grandTotal = 0;
+			con = new SqlConnection(Connection.GetConnectionString());
+			cmd = new SqlCommand("SellingReport", con);
+			cmd.Parameters.AddWithValue("@FromDate", fromDate);
+			cmd.Parameters.AddWithValue("@ToDate", toDate);
+			cmd.CommandType = CommandType.StoredProcedure;
+			sda = new SqlDataAdapter(cmd);
+			dt = new DataTable();
+			sda.Fill(dt);
+			if (dt.Rows.Count > 0)
+			{
+				foreach (DataRow dr in dt.Rows)
+				{
+					grandTotal += Convert.ToDouble(dr["TotalPrice"]);
+					//grandTotal = grandTotal + Convert.ToDouble(dr["TotalPrice"]);
+				}
+				lblTotal.Text = "Sold Cost: $" + grandTotal;
+				lblTotal.CssClass = "badge badge-primary";
 
+			}
+
+			rReport.DataSource = dt;
+			rReport.DataBind();
 		}
 
 		protected void btnSearch_Click(object sender, EventArgs e)
-        {
+		{
+			DateTime fromDate = Convert.ToDateTime(txtFromDate.Text);
+			DateTime toDate = Convert.ToDateTime(txtToDate.Text);
 
-        }
-
-	
+			if (toDate > DateTime.Now)
+			{
+				Response.Write("<script>alert('To date cannot be greater than today.');</script>");
+			}
+			else if (fromDate > toDate)
+			{
+				Response.Write("<script>alert('From date cannot be greater than To date.');</script>");
+			}
+			else
+			{
+				getReportData(fromDate, toDate);
+			}
+		}	
     }
 }
